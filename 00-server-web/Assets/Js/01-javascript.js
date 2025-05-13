71a0b59d3e9150d355ba30ed901a4aa7d3cbc3af
kevin26720 <kevin.villacis03@epn.edu.ec>
2025-05-13 18:30:32 -0500

    hasta inicio javascript-typescript

---
+++ 00-server-web/Assets/Js/01-javascript.js
@@ -0,0 +1,19 @@
+console.log("Mundo");
+//tipos de variables en javascript
+//Las variables son contenedores de datos que se pueden utilizar para almacenar información
+//Las variables mutables son aquellas que pueden cambiar su valor a lo largo del tiempo
+//Las variables inmutables son aquellas que no pueden cambiar su valor a lo largo del tiempo
+
+let nombre = "Juan"; // variable mutable
+nombre = "Pedro"; // se puede cambiar el valor de la variable
+nombre = 123; // se puede cambiar el tipo de dato de la variable
+//javascript es un lenguaje de tipado debil, no es necesario declarar el tipo de dato de la variable al momento de crearla
+nombre =  function (){
+    return 1;
+}
+
+//Las variables inmutables son aquellas que no pueden cambiar su valor a lo largo del tiempo
+const cedula = 123456789; // variable inmutable
+//cedula = 987654321; // no se puede cambiar el valor de la variable
+
+
