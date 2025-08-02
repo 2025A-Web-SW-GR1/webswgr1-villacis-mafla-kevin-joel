af0b173e1d68df8132ee9a6db2a29c57eae0d4a4
Kevin-Villacis <kevin.villacis03@epn.edu.ec>
2025-08-02 11:56:02 -0500

    actualizacion final guias

--- 02-servidor/ejemplo-serviodor/src/app.module.ts
+++ 02-servidor/ejemplo-serviodor/src/app.module.ts
@@ -3,13 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { CasaModule } from './casa/casa.module';
+import { AuthController } from './app.auth-controller';
 
 @Module({
   imports: [
     DatabaseModule,
     CasaModule
   ],
-  controllers: [AppController],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
